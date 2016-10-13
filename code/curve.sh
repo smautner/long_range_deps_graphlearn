b021d96f065315eefc256f8f0b73896618c27f89
S Mautner <mautner@cs.uni-freiburg.de>
2016-10-13 14:00:59 +0200

    autosync

--- code/curve.sh
+++ code/curve.sh
@@ -3,7 +3,14 @@
 #echo "##################################"
 #echo "#  CHECK PATHS IN THIS FILE       "
 #echo "##################################"
+CODEPATH="$PWD/scratch/nips2016/code"
+PYTHONPATH="$CODEPATH/deps/GraphLearn:$PYTHONPATH"
+PYTHONPATH="$CODEPATH/deps/EDeN:$PYTHONPATH"
+MPLCONFIGDIR="/home/mautner/mylittlepony/matplotlib$SGE_TASK_ID/crap"
 
+# we need to do this becuase the cluster sucks with env vars :(
+export PYTHONPATH
+export CODEPATH
 echo "i hope the cpu option works :) "
-cd "$CODEPATH/notebooks"; python curve.py $SGE_TASK_ID
+cd "$CODEPATH/notebooks"; python -m cProfile -o MYPROFILE curve.py 1
 
