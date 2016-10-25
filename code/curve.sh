a80fd4bb85b4deecf8b8d0a4f16731f2c827b160
S Mautner <mautner@cs.uni-freiburg.de>
2016-10-25 17:50:05 +0200

    autosync

--- code/curve.sh
+++ code/curve.sh
@@ -3,12 +3,11 @@
 #$ -pe smp 2
 #$ -l h_vmem=6G
 #$ -M mautner@cs.uni-freiburg.de
-
+mkdir $HOME/JOBZ/$JOB_NAME.$JOB_ID/out
+mkdir $HOME/JOBZ/$JOB_NAME.$JOB_ID/err
 #$ -o /home/mautner/JOBZ/$JOB_NAME.$JOB_ID/out/$TASK_ID.o
 #$ -e /home/mautner/JOBZ/$JOB_NAME.$JOB_ID/err/$TASK_ID.e
 
-mkdir $HOME/JOBZ/$JOB_NAME.$JOB_ID/out
-mkdir $HOME/JOBZ/$JOB_NAME.$JOB_ID/err
 
 #echo "##################################"
 #echo "#  CHECK PATHS IN THIS FILE       "
