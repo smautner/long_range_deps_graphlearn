8e167cf68ec1d8b459096658d0bae770d6bc98e0
smautner <mautner@cs.uni-freiburg.de>
2016-10-31 08:19:56 +0100

    realbars not has a result

--- code/paramopt.sh
+++ code/paramopt.sh
@@ -3,5 +3,9 @@
 #$ -pe smp 2
 #$ -l h_vmem=4G
 #$ -M mautner@cs.uni-freiburg.de
+#$ -o /home/mautner/JOBZ/paramopt_o/$JOB_NAME.$JOB_ID_o_$TASK_ID
+#$ -e /home/mautner/JOBZ/paramopt_e/$JOB_NAME.$JOB_ID_e_$TASK_ID
+
+
 cd notebooks; python -u parametersearch.py
 
