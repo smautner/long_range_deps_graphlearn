52d5d3f494f34513a7ee1fe412f15fc357a075b2
S Mautner <mautner@cs.uni-freiburg.de>
2016-11-11 16:04:37 +0100

    autosync

--- code/sync.sh
+++ code/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git commit -a -m 'autosync'
 git push
-ssh cluster 'cd /scratch/bi01/mautner/long_range_deps_graphlearn; git pull'
-ssh bipc    'cd /scratch/1/mautner/long_range_deps_graphlearn; git pull'
+ssh cluster 'cd /scratch/bi01/mautner/nips2016; git pull'
+ssh bipc    'cd /scratch/1/mautner/nips2016; git pull'
 
