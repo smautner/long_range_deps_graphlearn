b1e08bf1999998a8805852165fa78c051f53fa10
S Mautner <mautner@cs.uni-freiburg.de>
2016-11-11 14:38:52 +0100

    (re) moved things

--- code/nb_to_html.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PYTHONPATH="/scratch/1/mautner/nips2016/code/deps/GraphLearn:$PYTHONPATH"
-PYTHONPATH="/scratch/1/mautner/nips2016/code/deps/EDeN:$PYTHONPATH"
-echo $PYTHONPATH
-jupyter nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=None notebooks/curve.ipynb
--- code/setenv.sh
+++ code/setenv.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-
-
 CODEPATH="$PWD"
-#PYTHONPATH="/home/ikea/miniconda2/lib/python2.7/site-packages:$PYTHONPATH"
 PYTHONPATH="$CODEPATH/deps/GraphLearn:$PYTHONPATH"
 PYTHONPATH="$CODEPATH/deps/EDeN:$PYTHONPATH"
 PYTHONPATH="$CODEPATH/deps:$PYTHONPATH"
-#MPLCONFIGDIR="/home/mautner/mylittlepony/matplotlib$SGE_TASK_ID/crap"; export MPLCONFIGDIR
-
 export  CODEPATH
 export  PYTHONPATH
