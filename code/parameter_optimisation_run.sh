ed852229dba5de2c52ae16ecbbfd3468220a8a18
S Mautner <mautner@cs.uni-freiburg.de>
2016-11-16 19:30:00 +0100

    autosync

--- code/parameter_optimisation_run.sh
+++ code/parameter_optimisation_run.sh
@@ -7,4 +7,4 @@ mkdir -p /home/mautner/JOBZ/paramopt_e
 source ./setenv.sh
 
 #qsub -V -t 1-72 -pe smp 6  scratch/nips2016/code/curve.sh
-qsub -V -t 1-100 paramopt.sh
+qsub -V -t 1-100 parameter_optimisation.sh
